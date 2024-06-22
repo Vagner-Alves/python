891b5aeb4813bbf459d1a59a55d92cafcd28ae95
Vagner-Alves <vagneralves997@gmail.com>
2024-06-22 12:50:46 -0300

    tipos de dados

---
+++ 00 - Fundamentos/tipos_numericos.py
@@ -0,0 +1,19 @@
+'''
+NÚMEROS INTEIROS 
+Números inteiros são implementados pela classe int() e possuem precisão ilimitada.
+exemplos de números inteiros: 1, 10, 0 , -1, -10 .. -999999
+
+---------------------------------------------------------------------------------------------------
+
+NÚMEROS RACIONAIS OU PONTO FLUTUANTE
+Os números de ponto flutuante são usados para representar o conjunto dos números racionais e são implementados
+pela classe float()
+exemplos válidos de números racionais: 1.8, -10.4, 0.80 .... 9999.999
+
+'''
+
+print(2 + 2)
+print(3 * 3)
+
+print(3.3 * 2)
+print(12.8 - 10)
---
+++ tipo_booleano.py
@@ -0,0 +1,11 @@
+''' 
+BOOLEANOS
+
+é usado para representar verdadeiro ou falso , e é implementado pela classe bool(). Em python o tipo booleano é uma 
+subclasse do tipo int() , uma vez que qualquer número diferente de 0 representa Verdadeiro e 0 representa falso.
+são exemplos válidos de booleanos
+True e False
+
+
+'''
+
