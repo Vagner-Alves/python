676f998accfd3c020b30b51e49d5bccb12b1509b
Vagner-Alves <vagneralves997@gmail.com>
2024-06-21 23:32:43 -0300

    tipos de dados

---
+++ 00 - Fundamentos/tipos_de_dados.py
@@ -0,0 +1,7 @@
+str() #  String, lida com dados textuais
+int() # Int, lida com dados números
+float() # float, lida com números racionais
+bool() # bool (boolean) , lida com booleanos, ou seja, verdadeiro ou falso. 
+
+
+
