05ffed5b5660fad31d708258069cce70ef489dcc
Vagner-Alves <vagneralves997@gmail.com>
2024-06-21 23:40:21 -0300

    tipos de dados

--- 00 - Fundamentos/tipos_de_dados.py
+++ 00 - Fundamentos/tipos_de_dados.py
@@ -1,9 +1,11 @@
 # Python tem diversas classes para lidar com os mais variados tipos de dados.
 
-str() #  String: (  dados textuais )
-int() # Int: (  números inteiros )
-float() # float: ( números racionais )
-bool() # bool:  ( booleanos, ou seja, verdadeiro ou falso. ) 
+str()   # String: ( dados textuais )
+int()   # Int: (  números inteiros )
+float() # Float: ( números racionais )
+bool()  # Bool:  ( booleanos, ou seja, verdadeiro ou falso. ) 
+
+
 
 
 
