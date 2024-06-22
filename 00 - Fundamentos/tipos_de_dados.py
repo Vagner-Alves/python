f87bb4d00d8fa4608ff87c898db0c8b47abe8f62
Vagner-Alves <vagneralves997@gmail.com>
2024-06-22 00:02:15 -0300

    tipos de dados

--- 00 - Fundamentos/tipos_de_dados.py
+++ 00 - Fundamentos/tipos_de_dados.py
@@ -1,15 +1,16 @@
-#Tipos servem para as caracteristicas e comportamentos de um valor (Objeto) para o interpretador. 
+#Tipos servem para definir as caracteristicas e comportamentos de um valor (Objeto) para o interpretador / compilador. 
 # por exemplo, com o tipo int() é possível realizar operações matemáticas.
 # também irá informar ao interpretador (python) o quanto de memória será consumida para armazenalo.
 
-# Python tem diversas classes para lidar com os mais variados tipos de dados.
+# Python tem diversas classes (built-in) para lidar com os mais variados tipos de dados.
 
-str()   # String: ( dados textuais )
-int()   # Int: (  números inteiros )
-float() # Float: ( números racionais )
-bool()  # Bool:  ( booleanos, ou seja, verdadeiro ou falso. ) 
-
-
+str()                               # Texto
+int(), float(), complex()           # Números
+list(), tuple(), range()            # Sequencias 
+dict()                              # Mapa ( mapear valores)
+set(), frozenset()                  # Coleção 
+bool()                              # Booleano ( verdadeiro ou falso)
+bytes(), bytearray(), memoryview()  # Binário
 
 
 
