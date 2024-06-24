3a6faf0b12490ed77e4d06c194c7e9b94987137b
Vagner-Alves <vagneralves997@gmail.com>
2024-06-24 20:02:58 -0300

    excelente segunda feira

---
+++ 00 - Fundamentos/modo_interativo_do_python.py
@@ -0,0 +1,11 @@
+'''
+Modo Interativo -
+    o interpretador python pode executar um modo que permite ver em tempo real o resultado da execução
+    de um código.
+
+    existem duas formas de iniciar o modo interativo, chamando apenas o interpretador (python)
+    ou executando o script com a flag -i no terminal
+    exemplo: python -i app.py
+
+'''
+
