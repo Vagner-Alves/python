6db3ef297baf89886bcb63825de90a226fd5f315
Vagner-Alves <vagneralves997@gmail.com>
2024-07-07 23:53:18 -0300

    operações aritimeticas

--- 00 - Fundamentos/operadores_aritimeticos.py
+++ 00 - Fundamentos/operadores_aritimeticos.py
@@ -7,6 +7,18 @@ a seguinte ordem de operações é a correta:
 * Multiplicação e Divisão ( da esquerda para a direita)
 * Soma e Subtração (da esquerda para  a direita)
 
-
+O python obedece a ordem de operações
 
 '''
+
+numero_1 = 8
+numero_2 = 20
+
+print(numero_1  +  numero_2) # adição
+print(numero_1  -  numero_2) # subtração
+print(numero_1  *  numero_2) # multiplicação
+print(numero_1  /  numero_2) # divisão
+print(numero_1  // numero_2) # divisão inteira
+print(numero_1  %  numero_2) # Módulo
+print(numero_1  ** numero_2) # exponenciação
+
