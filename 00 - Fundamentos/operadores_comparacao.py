15cfe89005ef9c28f0c337167b3c80c8e5130807
Vagner-Alves <vagneralves997@gmail.com>
2024-07-08 02:09:53 -0300

    operadores de comparação

--- 00 - Fundamentos/operadores_comparacao.py
+++ 00 - Fundamentos/operadores_comparacao.py
@@ -1,5 +1,16 @@
 '''
 OPERADORES DE COMPARAÇÃO
-
+são operadores utilizando para comparar valores no python, o valor de retorno das comparações
+sempre será booleano, True quando verdadeiro e False quando falso.
 
 '''
+
+saldo_conta = 1000
+saque_conta = 230
+
+print( saldo_conta   ==  saque_conta)  # igual
+print( saldo_conta   !=  saque_conta ) # diferente
+print( saldo_conta   <   saque_conta ) # menor que
+print( saldo_conta   <=  saque_conta)  # menor ou igual
+print( saldo_conta   >   saque_conta)  # maior que
+print( saldo_conta   >=  saque_conta)  # maior ou igual
