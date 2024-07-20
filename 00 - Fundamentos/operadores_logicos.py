71c0e6ee200dd8f4a175f00f949236f3c2af1b3a
Vagner-Alves <vagneralves997@gmail.com>
2024-07-20 13:24:37 -0300

    operadores logicos

--- 00 - Fundamentos/operadores_logicos.py
+++ 00 - Fundamentos/operadores_logicos.py
@@ -1,4 +1,10 @@
 '''
 OPERADORES LÓGICOS
-
+são operadores usados utilizados em conjunto com os operadores de comparação, para montar
+uma expressão lógica. Quando um operador de comparação é utilizado, o resultado retornado é um booleano, 
+dessa forma podemos combinar operadores de comparação com os lógicos,
+exemplo:
+    comparação + lógico + comparação ... etc
 '''
+
+
