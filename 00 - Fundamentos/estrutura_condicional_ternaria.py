750945b9a6c68ff2d954d7f8c87a91dce2258d0b
Vagner-Alves <vagneralves997@gmail.com>
2024-08-07 21:44:11 -0300

    if ternário

--- 00 - Fundamentos/estrutura_condicional_ternaria.py
+++ 00 - Fundamentos/estrutura_condicional_ternaria.py
@@ -4,4 +4,9 @@ Permite escreve uma condição em uma única linha. Ele é composto por 3 partes
 a expressão retorne VERDADEIRO, a segunda parte é a EXPRESSÃO LÓGICA e a terceira parte é o RETORNO se a expressão for FALSA.
 
 '''
+saque_conta = 100
+saldo_conta = 875
 
+status = "Sucesso" if saque_conta <= saldo_conta  else "não há dinheiro o suficiente"
+
+print(f"{status} ao realizar o saque na conta. ")
