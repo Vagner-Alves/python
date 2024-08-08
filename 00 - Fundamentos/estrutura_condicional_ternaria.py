0a7d2408b9509a375df489bf46bcee70ac50ee40
Vagner-Alves <vagneralves997@gmail.com>
2024-08-07 21:40:48 -0300

    if ternário

---
+++ 00 - Fundamentos/estrutura_condicional_ternaria.py
@@ -0,0 +1,7 @@
+'''
+IF Ternário
+Permite escreve uma condição em uma única linha. Ele é composto por 3 partes, a primeira parte é o RETORNO, caso
+a expressão retorne VERDADEIRO, a segunda parte é a EXPRESSÃO LÓGICA e a terceira parte é o RETORNO se a expressão for FALSA.
+
+'''
+
