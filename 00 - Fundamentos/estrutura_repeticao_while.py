f64650fba044b25c2684b3d42a83610c48ac4155
Vagner-Alves <vagneralves997@gmail.com>
2024-08-30 13:09:28 -0300

    laço de repetição while

---
+++ 00 - Fundamentos/estrutura_repeticao_while.py
@@ -0,0 +1,8 @@
+'''
+WHILE
+
+O comando while é usado para repetir um bloco de código várias vezes.
+Faz sentido usar essa estrutura de repetição quando não sabemos o número exato de vezes
+que nosso bloco de código deve ser executado.
+'''
+
