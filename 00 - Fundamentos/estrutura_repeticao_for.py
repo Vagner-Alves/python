560e3a7ea2a2ac401afc742bb91c48ab7f9f9a22
Vagner-Alves <vagneralves997@gmail.com>
2024-08-29 22:10:01 -0300

    excelente quinta feira

---
+++ 00 - Fundamentos/estrutura_repeticao_for.py
@@ -0,0 +1,9 @@
+'''
+laço for
+Faz sentido usar o for quando sabemos o número exato de vezes que nosso bloco de código deve
+ser executado, ou quando queremos percorrer um objeto iteravél.
+'''
+
+texto = str(input("Digite qualquer texto: "))
+VOGAIS = 'aeiou'
+
