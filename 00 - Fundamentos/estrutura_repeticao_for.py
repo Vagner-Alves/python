7b18b207dfc7089e3775167ac5e5fe0d4a813b2c
Vagner-Alves <vagneralves997@gmail.com>
2024-08-30 09:55:00 -0300

    excelente sexta feira

--- 00 - Fundamentos/estrutura_repeticao_for.py
+++ 00 - Fundamentos/estrutura_repeticao_for.py
@@ -4,14 +4,5 @@ Faz sentido usar o for quando sabemos o número exato de vezes que nosso bloco d
 ser executado, ou quando queremos percorrer um objeto iteravél.
 '''
 
-texto = str(input("Digite qualquer texto: "))
-VOGAIS = 'AEIOU'
-
-for letras in texto:
-    if letras.upper() in VOGAIS:
-        print(letras, end=" ")
-
-else:
-    print()
 
 
