d7307437ac9ff1b93fa74b476cc794aad9cb24f7
Vagner-Alves <vagneralves997@gmail.com>
2024-08-29 21:12:32 -0300

    excelente quinta feira

---
+++ 00 - Fundamentos/estrutura_repeticao.py
@@ -0,0 +1,6 @@
+'''
+OQUE SÃO ESTRUTURAS DE REPETIÇÃO ?
+
+são estruturas utilizadas para repetir um trecho de código, um determinado número de vezes. Esse número pode ser conhecido
+previamente ou determinado através de uma expressão lógica.
+'''
