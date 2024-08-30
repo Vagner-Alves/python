6ea93a2af7b4b846f640845682fc27cf6c7591a2
Vagner-Alves <vagneralves997@gmail.com>
2024-08-30 10:01:41 -0300

    excelente sexta feira

--- 00 - Fundamentos/estrutura_repeticao_for.py
+++ 00 - Fundamentos/estrutura_repeticao_for.py
@@ -5,13 +5,13 @@ são estruturas utilizadas para repetir um trecho de código, um determinado nú
 previamente ou determinado através de uma expressão lógica.
 '''
 
-
-
 '''
 laço for
 Faz sentido usar o for quando sabemos o número exato de vezes que nosso bloco de código deve
 ser executado, ou quando queremos percorrer um objeto iteravél.
 '''
+
+
 
 
 
