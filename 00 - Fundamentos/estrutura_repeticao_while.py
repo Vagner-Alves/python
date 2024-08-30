7988bc8167fe63cfc796dbdab320f144e719719f
Vagner-Alves <vagneralves997@gmail.com>
2024-08-30 14:31:13 -0300

    laço de repetição while

--- 00 - Fundamentos/estrutura_repeticao_while.py
+++ 00 - Fundamentos/estrutura_repeticao_while.py
@@ -6,3 +6,15 @@ Faz sentido usar essa estrutura de repetição quando não sabemos o número exa
 que nosso bloco de código deve ser executado.
 '''
 
+opcao = -1
+
+while opcao != 0:
+    opcao = int(input("[1] Sacar \n[2] Extrato \n[0] Sair \n: "))
+
+    if opcao == 1:
+        print('Saque realizado com sucesso: ')
+    
+    elif opcao == 2:
+        print('Imprimindo Extrato da conta: ')
+else:
+    print("Obrigado por usar nosso sistema.") 
