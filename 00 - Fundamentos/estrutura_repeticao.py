86e91d3bb37cff822ac83a1b7c646ea7827c3b52
Vagner-Alves <vagneralves997@gmail.com>
2024-08-30 10:00:43 -0300

    excelente sexta feira

--- 00 - Fundamentos/estrutura_repeticao.py
+++
@@ -1,6 +0,0 @@
-'''
-OQUE SÃO ESTRUTURAS DE REPETIÇÃO ?
-
-são estruturas utilizadas para repetir um trecho de código, um determinado número de vezes. Esse número pode ser conhecido
-previamente ou determinado através de uma expressão lógica.
-'''
--- 00 - Fundamentos/estrutura_repeticao_for.py
+++ 00 - Fundamentos/estrutura_repeticao_for.py
@@ -1,4 +1,13 @@
 '''
+OQUE SÃO ESTRUTURAS DE REPETIÇÃO ?
+
+são estruturas utilizadas para repetir um trecho de código, um determinado número de vezes. Esse número pode ser conhecido
+previamente ou determinado através de uma expressão lógica.
+'''
+
+
+
+'''
 laço for
 Faz sentido usar o for quando sabemos o número exato de vezes que nosso bloco de código deve
 ser executado, ou quando queremos percorrer um objeto iteravél.
