196b8f32c693e951b01bcd5aee9f6543177b7db5
Vagner-Alves <vagneralves997@gmail.com>
2024-09-03 22:26:44 -0300

    excelente terça feira

--- 00 - Fundamentos/dominando strings e fatiamento/metodos_uteis.py
+++ 00 - Fundamentos/dominando strings e fatiamento/metodos_uteis.py
@@ -6,3 +6,12 @@ A classe string do python é famosa por ser rica em metódos e possuir uma inter
 Em algumas linguagens manipular sequencias de caracteres não é um trabalho trivial, porém, em python esse trabalho é muito simples.
 '''
 
+NOME_CURSO = "python"
+
+# Maiuscula, minuscula e Titulo
+
+print(NOME_CURSO.upper())  # Transforma os caracteres em maiusculos
+
+print(NOME_CURSO.lower()) # Transforma os caracteres em minusculos
+
+print(NOME_CURSO.title()) # Transforma a string em um titulo ( apenas primeira letra maiuscula )
