2968b57d45fe6f7f374cc151d09f7d09e779519a
Vagner-Alves <vagneralves997@gmail.com>
2024-09-03 22:16:45 -0300

    excelente terça feira

--- 00 - Fundamentos/dominando strings e fatiamento/metodos_uteis.py
+++ 00 - Fundamentos/dominando strings e fatiamento/metodos_uteis.py
@@ -1,5 +1,8 @@
 '''
 METÓDOS UTEIS DA CLASSE STRING
 
+A classe string do python é famosa por ser rica em metódos e possuir uma interface muito fácil de se trabalhar.
 
+Em algumas linguagens manipular sequencias de caracteres não é um trabalho trivial, porém, em python esse trabalho é muito simples.
 '''
+
