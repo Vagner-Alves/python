e939fcdca497efb785b332245bbcb6da0e57be4f
Vagner-Alves <vagneralves997@gmail.com>
2024-09-07 08:09:53 -0300

    excelente sabado

--- 00 - Fundamentos/dominando strings e fatiamento/fatiamento_strings.py
+++ 00 - Fundamentos/dominando strings e fatiamento/fatiamento_strings.py
@@ -1,5 +1,8 @@
 '''
 FATIAMENTO DE STRINGS
 
-
+é uma tecnica utilizada para retornar substrings ( partes da string original ) , informando o inicio, fim e passo [start: stop[step]]
 '''
+
+NOME  = "Vagner alves ferreira da silva"
+
