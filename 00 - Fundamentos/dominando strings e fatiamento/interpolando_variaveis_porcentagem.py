3255858a2231e29b2988a7a5faac85a1cbd274fc
Vagner-Alves <vagneralves997@gmail.com>
2024-09-07 20:14:01 -0300

    interpolando variaveis com %

---
+++ 00 - Fundamentos/dominando strings e fatiamento/interpolando_variaveis_porcentagem.py
@@ -0,0 +1,8 @@
+NOME = "Vagner Alves"
+IDADE = 27
+PROFISSAO = "DevOps"
+CURSO = "analise e desenvolvimento de sistemas"
+
+MENSAGEM = "Olá, meu nome é %s, tenho %d anos , minha profissão é %s e estou cursando %s." % (NOME, IDADE, PROFISSAO, CURSO)
+
+print(MENSAGEM)
