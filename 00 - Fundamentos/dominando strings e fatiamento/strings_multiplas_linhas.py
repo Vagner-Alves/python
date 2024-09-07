b83fc302e15b1ce014b46e80b76a3d93414184c4
Vagner-Alves <vagneralves997@gmail.com>
2024-09-07 09:56:04 -0300

    strings em multiplas linhas

---
+++ 00 - Fundamentos/dominando strings e fatiamento/strings_multiplas_linhas.py
@@ -0,0 +1,15 @@
+'''
+STRINGS EM MULTIPLAS LINHAS
+essas strings são atribuidas informandos 3 aspas simples ou duplas durante a atribuição.
+Elas podem ocupar várias linhas de código, e todos os espaços em branco são incluído na string final.
+'''
+
+NOME = "Vagner Alves"
+MENSAGEM = f'''
+    Olá, meu nome é {NOME} e estou aprendendo python!
+        - no curso da dio!
+                - e softex
+
+'''
+
+print(MENSAGEM)
