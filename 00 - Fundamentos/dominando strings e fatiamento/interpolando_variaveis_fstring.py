3b9eaceebfb62860dab139889f07a9a97b257889
Vagner-Alves <vagneralves997@gmail.com>
2024-09-07 20:20:37 -0300

    interpolando variaveis com %

---
+++ 00 - Fundamentos/dominando strings e fatiamento/interpolando_variaveis_fstring.py
@@ -0,0 +1,6 @@
+NOME = "Vagner Alves"
+IDADE = 27
+PROFISSAO = "DevOps"
+CURSO = "analise e desenvolvimento de sistemas"
+
+MENSAGEM =  f"Olá, meu nome é {NOME} , tenho {IDADE} anos , minha profissão é {PROFISSAO} e estou cursando {CURSO}"
