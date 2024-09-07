b0b2dd9fd115eabaa57caece592af02d37c48e33
Vagner-Alves <vagneralves997@gmail.com>
2024-09-07 20:53:27 -0300

    interpolando variaveis com format

--- 00 - Fundamentos/dominando strings e fatiamento/interpolando_variaveis_fstring.py
+++ 00 - Fundamentos/dominando strings e fatiamento/interpolando_variaveis_fstring.py
@@ -2,5 +2,10 @@ NOME = "Vagner Alves"
 IDADE = 27
 PROFISSAO = "DevOps"
 CURSO = "analise e desenvolvimento de sistemas"
+INVESTIMENTO = 18.8888
 
 MENSAGEM =  f"Olá, meu nome é {NOME} , tenho {IDADE} anos , minha profissão é {PROFISSAO} e estou cursando {CURSO}"
+MENSAGEM_1 = f"Meus investimentos no último dia, renderam {INVESTIMENTO:.2f}"
+
+print(MENSAGEM_1)
+print(MENSAGEM)
