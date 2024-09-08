523779806937006dd24d1eb415583753595ef4fc
Vagner-Alves <vagneralves997@gmail.com>
2024-09-07 21:04:47 -0300

    interpolando variaveis com fstring

--- 00 - Fundamentos/dominando strings e fatiamento/interpolando_variaveis_fstring.py
+++ 00 - Fundamentos/dominando strings e fatiamento/interpolando_variaveis_fstring.py
@@ -3,9 +3,13 @@ IDADE = 27
 PROFISSAO = "DevOps"
 CURSO = "analise e desenvolvimento de sistemas"
 INVESTIMENTO = 18.8888
+SALDO_CONTA = 10000
 
 MENSAGEM =  f"Olá, meu nome é {NOME} , tenho {IDADE} anos , minha profissão é {PROFISSAO} e estou cursando {CURSO}"
 MENSAGEM_1 = f"Meus investimentos no último dia, renderam {INVESTIMENTO:.2f}"
+MENSAGEM_3 = f"Meu primeiro salário, R${SALDO_CONTA:10.2f}"
+
 
 print(MENSAGEM_1)
 print(MENSAGEM)
+print(MENSAGEM_3)
