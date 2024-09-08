ae2b293e502a7f3c868730e5675c6820410ea206
Vagner-Alves <vagneralves997@gmail.com>
2024-09-08 17:03:03 -0300

    excelente fim de semana

--- 01 - Estruturas de Dados/trabalhando com listas em python/criando_listas.py
+++ 01 - Estruturas de Dados/trabalhando com listas em python/criando_listas.py
@@ -1,5 +1,8 @@
 '''
 LISTAS EM PYTHON
 listas em python podem armarzenar de maneira sequencial qualquer tipo de objeto.
+podemos criar listas utilizando o constructor list, a funçõa range ou colocando valores
+separados por vírgula dentro de colchetes. Listas são objetos mutáveis, portanto podemos
+alterar seus valores após a criação.
 
 '''
