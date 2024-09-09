c5a4a42f702fac8137dd386e44973397bda244ab
Vagner-Alves <vagneralves997@gmail.com>
2024-09-09 10:07:20 -0300

    excelente segunda feira

---
+++ 01 - Estruturas de Dados/trabalhando com listas em python/listas_aninhadas.py
@@ -0,0 +1,6 @@
+'''
+LISTAS ANINHADAS
+listas podem armazenar todos os tipos de objeto python, portanto podemos ter listas que armazenam outras listas.
+Com isso podemos criar estruturas bidimensionais ( tabelas ), e acessar informando os indices de linha e coluna.
+'''
+
