3bf531a5560cd6df06aab588a64e6d246abab5d1
Vagner-Alves <vagneralves997@gmail.com>
2024-09-09 09:57:20 -0300

    excelente segunda feira

--- 01 - Estruturas de Dados/trabalhando com listas em python/criando_listas.py
+++ 01 - Estruturas de Dados/trabalhando com listas em python/criando_listas.py
@@ -7,12 +7,22 @@ alterar seus valores após a criação.
 
 '''
 
-letras       = list("curso de python")                     # vai criar uma lista com cada caractere da string.
-numeros      = range(10)                                   # vai criar uma lista com números de 0 a 10.
-lista_frutas = ["maçã","banana","uva","pessego","manga"]   # vai criar uma lista com os elementos dentro dos colchetes.
-carrinho_compras = ['tenis branco',120, 'tenis preto',120] # vai criar uma lista com diversos tipos de objeto ( string e integer).
+letras            = list("curso de python")                     # vai criar uma lista com cada caractere da string.
+numeros           = range(10)                                   # vai criar uma lista com números de 0 a 10.
+lista_frutas      = ["maçã","banana","uva","pessego","manga"]   # vai criar uma lista com os elementos dentro dos colchetes.
+carrinho_compras  = ['tenis branco',120, 'tenis preto',120]     # vai criar uma lista com diversos tipos de objeto ( string e integer).
 
 
 print(letras)
 print(numeros)
 print(lista_frutas)
+print(" ")
+
+'''
+ACESSO DIRETO
+
+A lista é uma sequencia, portanto podemos acessar seus dados utilizando indices.
+Contamos o indice de determinada sequencia apartir de zero.
+'''
+
+print(lista_frutas[0])
