3ccd359485959567ed13939288f2a8ffaaa349b5
Vagner-Alves <vagneralves997@gmail.com>
2024-09-09 11:00:38 -0300

    enumerate

--- 01 - Estruturas de Dados/funcao_enumerate.py
+++ 01 - Estruturas de Dados/funcao_enumerate.py
@@ -4,3 +4,8 @@ ENUMERATE
 as vezes é necessário saber qual é o índice do objeto dentro do laço for.
 Para isso podemos usar a funçõa enumerate. 
 '''
+
+carros = ["GOl","CELTA", "KWID ( carro descartável )"]
+
+for indice, carro in enumerate(carros):
+    print(f"{indice} -  {carro}")
