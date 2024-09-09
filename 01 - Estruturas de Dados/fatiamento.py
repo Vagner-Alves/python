9a45c3ca691d881a566cde0e42bd5d3d84e1011b
Vagner-Alves <vagneralves997@gmail.com>
2024-09-09 10:44:39 -0300

    excelente segunda feira

--- 01 - Estruturas de Dados/fatiamento.py
+++
@@ -1,8 +0,0 @@
-'''
-FATIAMENTO
-além de acessar elementos diretamente , podemos extrair um conjunto de valores de uma sequencia.
-Para isso basta passar o indice inicial e /ou final para acessar o conjunto. Podemos ainda informar
-quantas posições o cursos deve "pular" no processo.
-
-'''
-
---
+++ 01 - Estruturas de Dados/trabalhando com listas em python/fatiamento.py
@@ -0,0 +1,16 @@
+'''
+FATIAMENTO
+além de acessar elementos diretamente , podemos extrair um conjunto de valores de uma sequencia.
+Para isso basta passar o indice inicial e /ou final para acessar o conjunto. Podemos ainda informar
+quantas posições o cursos deve "pular" no processo.
+
+'''
+
+lista = list('python')
+
+print(lista[2:])                # pega as últimas 3 letras
+print(lista[:2])                # pega as duas primeiras letras
+print(lista[1:3])               # 
+print(lista[0:3:2])
+print(lista[::])               # tira uma cópia da lista
+print(lista[::-1])
