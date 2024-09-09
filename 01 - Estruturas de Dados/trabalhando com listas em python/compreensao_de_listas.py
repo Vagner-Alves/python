a4e12e38a29fb213eb7918a49c1f8629fb96e091
Vagner-Alves <vagneralves997@gmail.com>
2024-09-09 12:55:11 -0300

    enumerate

---
+++ 01 - Estruturas de Dados/trabalhando com listas em python/compreensao_de_listas.py
@@ -0,0 +1,8 @@
+'''
+COMPREENSÃO DE LISTAS
+
+A compreensão de listas oferece uma sintaxe mais curta quando voce deseja: criar
+uma nova lista com base nos valores de uma lista existente (filtro) ou gerar uma nova lista
+aplicando alguma modificação nos elementos de uma lista existente.
+'''
+
