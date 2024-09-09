6035a0324bd49a39119a309abd4683fffa9d22c6
Vagner-Alves <vagneralves997@gmail.com>
2024-09-09 10:30:15 -0300

    excelente segunda feira

--- 01 - Estruturas de Dados/fatiamento.py
+++ 01 - Estruturas de Dados/fatiamento.py
@@ -1,6 +1,8 @@
 '''
 FATIAMENTO
-
-
+além de acessar elementos diretamente , podemos extrair um conjunto de valores de uma sequencia.
+Para isso basta passar o indice inicial e /ou final para acessar o conjunto. Podemos ainda informar
+quantas posições o cursos deve "pular" no processo.
 
 '''
+
