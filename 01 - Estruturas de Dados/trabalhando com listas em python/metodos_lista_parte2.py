1f03784ee0e4e08fecf802fac162ffc02d5ca0f1
Vagner-Alves <vagneralves997@gmail.com>
2024-09-09 14:41:13 -0300

    metodos de lista

--- 01 - Estruturas de Dados/trabalhando com listas em python/metodos_lista_parte2.py
+++ 01 - Estruturas de Dados/trabalhando com listas em python/metodos_lista_parte2.py
@@ -8,7 +8,15 @@ a lista por padrão já vem organizada como uma PILHA
 lista = ['uva','aveia', 'maçã', 'banana', 'pera']
 
 # pop sempre irá remover apartir do último elemento 
-print(lista.pop() ) # remove pera
+
+print(lista.pop() )        # remove pera
+print(lista.pop() )        # remove banana
+print(lista.pop() )        # remove maçã
+print(lista.pop(0))        # remove pelo índice ( uva )
 
 
+# removendo elementos especificos da lista ( pelo nome )
 
+linguagens_programacao = ["Python", "Java", "javaScript", "Ruby"]
+linguagens_programacao.remove("Ruby")
+print(linguagens_programacao)
