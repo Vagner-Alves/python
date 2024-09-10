c758dcbaa4332fdc24eb8c485ff530d5ec05597e
Vagner-Alves <vagneralves997@gmail.com>
2024-09-10 17:48:50 -0300

    excelente terça feira

--- 01 - Estruturas de Dados/conhecendo tuplas em python/tuplas_aninhadas.py
+++ 01 - Estruturas de Dados/conhecendo tuplas em python/tuplas_aninhadas.py
@@ -5,5 +5,8 @@ tuplas podem armazenar todos os tipos de objeto python, portanto podemos ter tup
 com isso podemos criar estruturas bidimensionais ( tabelas ) , e acessar informando os índices de linha e coluna.
 '''
 
-lista_frutas = ("morango", "maçã", "uva", "pessego", "banana", "jaca")
+lista_frutas = ("morango", "maçã", "uva", "pessego", "banana", "jaca",)
+
 print(lista_frutas)
+
+paises = ("Brasil",) # sempre colocar uma virgula ao final para não confundir o compilador python.
