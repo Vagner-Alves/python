d4270ea4395ff49f2fedfdb0559d15468a9f259c
Vagner-Alves <vagneralves997@gmail.com>
2024-09-22 11:15:21 -0300

    conjuntos em python

---
+++ 01 - Estruturas de Dados/conjuntos/acessando_dados.py
@@ -0,0 +1,5 @@
+'''
+Conjuntos em python não suportam indexação, nem fatiamento, caso queira acessar seus valores é necessário
+converter o conjunto para uma lista.
+'''
+
--- 01 - Estruturas de Dados/conjuntos/conjuntos.py
+++ 01 - Estruturas de Dados/conjuntos/conjuntos.py
@@ -1,5 +1,10 @@
 '''
 Um Set é uma coleção que não possui objetos repetidos, usamos set para representar conjuntos matemáticos
-ou eliminar itens duplicados de um iteravél.
+ou eliminar itens duplicados de um iteravél. O Set não garante a ordem dos elementos.
 '''
 
+conjunto_numerico = set([1,2,3,3,4,6,5,2])
+linguagens_programacao = set(["java","python","js","java","python"])
+
+print(conjunto_numerico)
+print(linguagens_programacao)
