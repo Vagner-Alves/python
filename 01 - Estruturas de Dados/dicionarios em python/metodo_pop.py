acaf28268368961ae0cda7c370be0dd919a48325
Vagner-Alves <vagneralves997@gmail.com>
2024-12-07 18:18:53 -0300

    dicionario metodos

--- 01 - Estruturas de Dados/dicionarios em python/metodo_pop.py
+++ 01 - Estruturas de Dados/dicionarios em python/metodo_pop.py
@@ -1,5 +1,6 @@
 '''
 O pop vai remover o valor de um dicionário, após remover o valor ele irá retornalo
+
 '''
 
 agenda_telefone = {"Vagner": {"telefone": 819889998, "email":"vagneralves@email.com"}, 
@@ -13,3 +14,12 @@ print(agenda_telefone)
 # definindo um valor de retorno padrão
 resultado = agenda_telefone.pop("priscila", "chave nao encontrada")
 print(resultado)
+
+# o metodo popitem é tem a mesma função, porém remove os dados em sequencia 
+
+carrinho_compras = {"banana": {"preco":0.15, "quantidade":8},"aveia": {"preco": 1.12, "quantidade": 1}}
+
+carrinho_compras.popitem()
+print(carrinho_compras)
+carrinho_compras.popitem()
+print(carrinho_compras)
