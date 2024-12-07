3ecb0d29eaa90e3959e4a9f2caba8094a2abbbde
Vagner-Alves <vagneralves997@gmail.com>
2024-12-07 19:01:12 -0300

    dicionario metodos

---
+++ 01 - Estruturas de Dados/dicionarios em python/metodo_values.py
@@ -0,0 +1,13 @@
+'''
+esse metodo retorna todos os valores de um dicionario
+'''
+
+
+contatos = {
+    "vagneralves997@gmail.com": {"nome":"vagner", "telefone":"(xx) xxxx-xxxx"},
+    "alguem@email.com": {"nome":"alguém", "telefone":"33 3333-3333"},
+    "ff@email.com": {"nome": "ff", "telefone": "44 4444-4444"},
+    "yy@hotmail.com": {"nome": "xx", "telefone": "55 5555-5555"}
+}
+
+print(contatos.values())
