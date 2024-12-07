09bbf522942c00ad4ce4d7605d66ada130792154
Vagner-Alves <vagneralves997@gmail.com>
2024-12-07 18:12:50 -0300

    dicionario metodos

--- 01 - Estruturas de Dados/dicionarios em python/metodo_pop.py
+++ 01 - Estruturas de Dados/dicionarios em python/metodo_pop.py
@@ -1,9 +1,15 @@
 '''
-O pop vai remover o valor de um dicionário
+O pop vai remover o valor de um dicionário, após remover o valor ele irá retornalo
 '''
 
-agenda_telefone = {"Vagner": {"telefone": 819889998, "email":"vagneralves@email.com"}}
+agenda_telefone = {"Vagner": {"telefone": 819889998, "email":"vagneralves@email.com"}, 
+"priscila":{"telefone": 8888188, "email":"pri@email.com"}
+}
 print(agenda_telefone)
 
 agenda_telefone.pop("Vagner")
 print(agenda_telefone)
+
+# definindo um valor de retorno padrão
+resultado = agenda_telefone.pop("priscila", "chave nao encontrada")
+print(resultado)
