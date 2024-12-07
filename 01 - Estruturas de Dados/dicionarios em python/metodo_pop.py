8c543fa5f095941c38db1c117ac75948eabe8fd1
Vagner-Alves <vagneralves997@gmail.com>
2024-12-07 18:06:53 -0300

    dicionario metodos

---
+++ 01 - Estruturas de Dados/dicionarios em python/metodo_pop.py
@@ -0,0 +1,9 @@
+'''
+O pop vai remover o valor de um dicionário
+'''
+
+agenda_telefone = {"Vagner": {"telefone": 819889998, "email":"vagneralves@email.com"}}
+print(agenda_telefone)
+
+agenda_telefone.pop("Vagner")
+print(agenda_telefone)
