e3c0edd834c7f2a0e8640bb270d49f7f972335a4
Vagner-Alves <vagneralves997@gmail.com>
2024-12-08 17:44:49 -0300

    funções

---
+++ 01 - Estruturas de Dados/dominando funcoes python/retorno_funcao.py
@@ -0,0 +1,17 @@
+'''
+para retorna um valor, utilizamos a palavra reservada return.
+Toda função python retorna None por padrão. Diferente de outras linguagens
+de programação, em python uma função pode retornar mais de um valor.
+'''
+
+def calcular_total(numeros):
+    return sum(numeros)
+
+calcular_total([1,1,3,4])
+
+def retornar_antecessor_sucessor(numero):
+    antecessor = numero -1
+    sucessor = numero +1
+    return antecessor, sucessor
+
+retornar_antecessor_sucessor(8)
