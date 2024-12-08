86fae1b4d82b54f8adf35f4e80e4bc1f430b680d
Vagner-Alves <vagneralves997@gmail.com>
2024-12-08 17:20:06 -0300

    funções

---
+++ 01 - Estruturas de Dados/dominando funcoes python/primeira_funcao.py
@@ -0,0 +1,6 @@
+'''
+função é um bloco de código identificado por um nome e pode receber uma lista de parametros,
+esses parametros podem ou não ter valores padrões. Usar funções torna o código mais légivel e possibilita
+o reaproveitamento do código. Programar baseado em funções , é o mesmo que dizer que estamos programando de 
+maneira estruturada.
+'''
