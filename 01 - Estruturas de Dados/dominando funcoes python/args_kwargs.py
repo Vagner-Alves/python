cd805ff31519789381d99462cef6f96cc1189cc3
Vagner-Alves <vagneralves997@gmail.com>
2024-12-08 18:22:56 -0300

    funções

--- 01 - Estruturas de Dados/dominando funcoes python/args_kwargs.py
+++ 01 - Estruturas de Dados/dominando funcoes python/args_kwargs.py
@@ -2,5 +2,7 @@
 podemos combinar parametros obrigatórios com args e kwargs. Quando esses
 são definidos ( *args, *kwargs) , o método recebe os valores como tupla e dicionário
 respectivamente.
+é bom observar o seguinte, os nomes args e kwargs pouco importam, na verdade voce pode
+usar oque quiser , o python vai se atentar aos simbolos * e **
 '''
 
