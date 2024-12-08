d3be30cd6f57b012f05fc85ff9ccda5c099618ca
Vagner-Alves <vagneralves997@gmail.com>
2024-12-08 18:30:08 -0300

    funções

--- 01 - Estruturas de Dados/dominando funcoes python/args_kwargs.py
+++ 01 - Estruturas de Dados/dominando funcoes python/args_kwargs.py
@@ -6,3 +6,10 @@ respectivamente.
 usar oque quiser , o python vai se atentar aos simbolos * e **
 '''
 
+def exibir_poema(data_extenso, *args, **kwargs):
+    texto = "\n".join(args)
+    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
+    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    print(mensagem)
+
+exibir_poema("o ano de 2024", "foi belo e cheio de conquistas", autor="Vagner Alves", ano=2024)
