b9eb1765aabae33a84e53880feab21a31592ec1a
Vagner-Alves <vagneralves997@gmail.com>
2024-12-08 22:36:00 -0300

    funções

--- 01 - Estruturas de Dados/dominando funcoes python/parametro_posicao.py
+++ 01 - Estruturas de Dados/dominando funcoes python/parametro_posicao.py
@@ -6,4 +6,8 @@ pelo qual argumentos possam ser passados, assim um desenvolvedor
 precisa apenas olhar para a definição da função para determinar
 se os itens são passados por posição, por posição e nome, ou por nome.
 '''
+# é util caso queira mudar o nome do parametro depois ( antes da barra)
+def criar_carro(modelo, ano, placa, /, marca, motor, combustivel):
+    print(modelo, ano, placa, marca,motor, combustivel)
 
+criar_carro("Palio azul", 1997, "123-AB", marca="fiat", motor="V8", combustivel="alcool") # forma correta
