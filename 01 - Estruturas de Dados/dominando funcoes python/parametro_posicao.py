eaab58989e5dd7a0d7903e276a60c11469f64146
Vagner-Alves <vagneralves997@gmail.com>
2024-12-08 22:26:24 -0300

    funções

---
+++ 01 - Estruturas de Dados/dominando funcoes python/parametro_posicao.py
@@ -0,0 +1,9 @@
+'''
+por padrão, argumentos podem ser passados para uma função python
+tanto por posição quanto expplicitamente pelo nome.
+Para uma melhor legibilidade e desempenho, faz sentido restringir a maneira
+pelo qual argumentos possam ser passados, assim um desenvolvedor
+precisa apenas olhar para a definição da função para determinar
+se os itens são passados por posição, por posição e nome, ou por nome.
+'''
+
