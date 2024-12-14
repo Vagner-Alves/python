6b6c3ef5e06ca4fa809cd4cab1fade43d1102cc0
Vagner-Alves <vagneralves997@gmail.com>
2024-12-14 19:41:17 -0300

    construtores e destrutores

--- 02 - programacao orientada a objetos/introducao/metodo_construtor.py
+++ 02 - programacao orientada a objetos/introducao/metodo_construtor.py
@@ -4,3 +4,4 @@ sempre é executado quando uma nova instancia da classe é criada. Nesse método
 o estado do nosso objeto. Para declarar o método construtor da classe, criamos um método com o nome
 __init__
 '''
+
