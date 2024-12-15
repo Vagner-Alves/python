1a5aad0fcfd9173f3ee78ace8500a0506f99e5c0
Vagner-Alves <vagneralves997@gmail.com>
2024-12-15 09:07:26 -0300

    construtores e destrutores

---
+++ 02 - programacao orientada a objetos/introducao/metodo_destrutor.py
@@ -0,0 +1,7 @@
+'''
+Método Destrutor
+o método destrutor sempre é executado quando uma instancia
+ (objeto) é destruída. Destrutores em python não são tão necessários quanto em C++
+ porque o python tem um coletor de lixo que lida com o gerenciamento de memória automaticamente.
+ Para declarar o método destrutor da classe, criamos um método com o nome __del__
+'''
