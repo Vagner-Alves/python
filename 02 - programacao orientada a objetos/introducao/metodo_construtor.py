622b287e366277457b0d7576184bc54bebb78f97
Vagner-Alves <vagneralves997@gmail.com>
2024-12-15 09:43:53 -0300

    construtores e destrutores

--- 02 - programacao orientada a objetos/introducao/metodo_construtor.py
+++ 02 - programacao orientada a objetos/introducao/metodo_construtor.py
@@ -5,3 +5,14 @@ o estado do nosso objeto. Para declarar o método construtor da classe, criamos 
 __init__
 '''
 
+class Impressora():
+    def __init__(self, modelo, marca, conectividade):
+        self.modelo = modelo
+        self.marca = marca
+        self.conectividade = conectividade
+
+
+# init é o método construtor da classe Impressora e nele são definidos os atributos da classe
+
+impressora = Impressora("HP 581", "HP", "WIFI- USB")
+print(impressora)
--- 02 - programacao orientada a objetos/introducao/metodo_destrutor.py
+++ 02 - programacao orientada a objetos/introducao/metodo_destrutor.py
@@ -5,3 +5,16 @@ o método destrutor sempre é executado quando uma instancia
  porque o python tem um coletor de lixo que lida com o gerenciamento de memória automaticamente.
  Para declarar o método destrutor da classe, criamos um método com o nome __del__
 '''
+
+class Impressora():
+    def __init__(self, modelo, marca, conectividade):
+        self.modelo = modelo
+        self.marca = marca
+        self.conectividade = conectividade
+    
+    def __del__(self):
+        print("instancia", self.__class__.__name__, "deletada")
+
+impressora = Impressora("HP 581", "HP", "WIFI- USB")
+impressora.__del__()
+        
