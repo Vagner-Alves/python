bed51c6d800d98fdf54397bee25356f13ee84fea
Vagner-Alves <vagneralves997@gmail.com>
2024-12-23 09:02:00 -0300

    heranca simples

---
+++ 02 - programacao orientada a objetos/03 - encapsulamento/recursos_publicos_e_privados.py
@@ -0,0 +1,14 @@
+'''
+Em linguagens como java e C++, existem palavras reservadas para definir
+o nível de acesso aos atributos e métodos da classe. Em python não
+temos palavras reservadas, porém usamos convenções no nome do recurso, para
+definir se a variável é pública ou privada.
+
+Público: Pode ser acessado de fora da classe.
+Privado: Só pode ser acessado pela classe.
+ 
+'''
+
+
+
+
