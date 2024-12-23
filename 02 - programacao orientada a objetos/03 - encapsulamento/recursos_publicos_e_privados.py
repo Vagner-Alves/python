179bb02ab2dd069002cbafec524ea95611d59ff4
Vagner-Alves <vagneralves997@gmail.com>
2024-12-23 09:27:25 -0300

    heranca simples

--- 02 - programacao orientada a objetos/03 - encapsulamento/recursos_publicos_e_privados.py
+++ 02 - programacao orientada a objetos/03 - encapsulamento/recursos_publicos_e_privados.py
@@ -6,8 +6,29 @@ definir se a variável é pública ou privada.
 
 Público: Pode ser acessado de fora da classe.
 Privado: Só pode ser acessado pela classe.
- 
+
+todos os recursos são públicos, a menos que o nome inicie com underline.
+Ou seja, o interpretador python não irá garantir a proteção do recurso, mas por ser uma convenção
+amplamente adotada na comunidade, quando encontramos uma variável e/ou método com nome iniciado por underline
+, sabemos que não deveríamos manipular o seu valor diretamente, ou invocar método fora do escopo da classe.
 '''
+
+class Conta:
+    def __init__(self, numero_agencia, saldo=0):
+        self._saldo = saldo
+        self.numero_agencia = numero_agencia
+    
+    def depositar(self, valor):
+        # faz de conta que está implementado
+
+        self._saldo += valor
+    
+    def sacar(self, valor):
+        # mesma coisa
+
+        self._saldo -= valor
+
+conta = Conta("1234", 100)
 
 
 
