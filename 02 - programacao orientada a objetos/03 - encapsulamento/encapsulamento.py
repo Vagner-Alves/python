54b43e499337b7d6e1b549d341ba66ea8b1cfe21
Vagner-Alves <vagneralves997@gmail.com>
2024-12-23 08:27:25 -0300

    heranca simples

---
+++ 02 - programacao orientada a objetos/03 - encapsulamento/encapsulamento.py
@@ -0,0 +1,8 @@
+'''
+PROTEÇÃO DE ACESSO
+o encapsulamento é um dos conceitos fundamentais de programação orientada a objetos.
+Ele descreve a ideia de agrupar dados e os métodos que manipulam esses dados em uma unidade.
+Isso impões restrições ao acesso direto a variaáveis e métodos e pode evitar a modificação acidental de dados.
+Para evitar alterações acidentais, a variável de um objeto só pode ser alterada pelo método desse objeto.
+'''
+
