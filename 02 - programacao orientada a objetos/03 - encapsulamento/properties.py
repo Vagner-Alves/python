0655f4a88f183e8bf005009d8bdb852e5f1bc612
Vagner-Alves <vagneralves997@gmail.com>
2024-12-28 19:47:19 -0300

    properties em python

--- 02 - programacao orientada a objetos/03 - encapsulamento/properties.py
+++ 02 - programacao orientada a objetos/03 - encapsulamento/properties.py
@@ -7,3 +7,24 @@ Voce pode usar atributos gerenciados , também conhecidos como properties ( prop
 , quando precisar modificar sua implementação interna sem alterar a API pública da classe.
 '''
 
+class Foo:
+    def __init__(self, x=None):
+        self._x = x
+    
+    @property
+    def x(self):
+        return self._x or 0
+
+    @x.setter
+    def x(self, value):
+        _x = self._x or 0
+        _value = value or 0
+        self._x = _x + _value
+    
+    @x.deleter
+    def x(self):
+        self._x = -1
+
+foo = Foo(10)
+    
+
