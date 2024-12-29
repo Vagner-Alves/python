c10c47dc81ec2991f21b46336c17ba17b4a7d346
Vagner-Alves <vagneralves997@gmail.com>
2024-12-28 23:47:43 -0300

    polimorfismo

--- 02 - programacao orientada a objetos/04 - polimorfismo/polimorfismo.py
+++ 02 - programacao orientada a objetos/04 - polimorfismo/polimorfismo.py
@@ -2,4 +2,12 @@
 A palavra polimorfismo significa ter muitas formas. Na progrmação,
 polimorfismo significa o mesmo nome da função, mas assinaturas diferentes
 sendo usado para tipos diferentes.
+
+MESMO MÉTODO COM COMPORTAMENTO DIFERENTE
+
+na herança, a classe filha herda os métodos da classe pai. No entanto,
+é possível modificar um método em uma classe filha herdada da
+classe pai. Isso é particurlamente útil nos casos em que o método herdado
+da classe pai não se encaixa perfeitamente na classe filha.
 '''
+
