2565d9db2893f90ced54dd33cec798a795535319
Vagner-Alves <vagneralves997@gmail.com>
2024-12-28 23:34:28 -0300

    polimorfismo

---
+++ 02 - programacao orientada a objetos/04 - polimorfismo/polimorfismo.py
@@ -0,0 +1,5 @@
+'''
+A palavra polimorfismo significa ter muitas formas. Na progrmação,
+polimorfismo significa o mesmo nome da função, mas assinaturas diferentes
+sendo usado para tipos diferentes.
+'''
