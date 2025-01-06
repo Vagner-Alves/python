f77c15ae0ff5a2dd70c17cb6f6bb5cf73d78f223
Vagner-Alves <vagneralves997@gmail.com>
2025-01-06 13:29:19 -0300

    interfaces e classes abstratas em python

--- 02 - programacao orientada a objetos/05 - interfaces e classes abstratas/variaveis_de_classe_e_instancia.py
+++ 02 - programacao orientada a objetos/05 - interfaces e classes abstratas/variaveis_de_classe_e_instancia.py
@@ -6,3 +6,15 @@ e de instancia. Atributos de instancia são diferentes para cada objeto
 ( cada objeto tem uma cópia), já os atributos de classe
 são compartilhados entre os objetos.
 '''
+
+class Estudante:
+    escola = "IFPE"
+
+    def __init__(self, nome, numero):
+        self.nome =  nome
+        self.numero = numero
+    
+    def __str__():
+        return f"nome: {self.nome}  numero: {self.numero}"
+
+vagner = Estudante("Vagner", "20222ADS-PM0202")
