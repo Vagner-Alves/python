1cc4bc4fb4bd1a27e6043746b47a3060bebe9dec
Vagner-Alves <vagneralves997@gmail.com>
2025-01-06 16:25:56 -0300

    interfaces e classes abstratas em python

--- 02 - programacao orientada a objetos/05 - interfaces e classes abstratas/classes_abstratas.py
+++ 02 - programacao orientada a objetos/05 - interfaces e classes abstratas/classes_abstratas.py
@@ -1,25 +1,52 @@
-from abc import ABC, abstractmethod, property
+from abc import ABC, abstractmethod
 
 class ControleRemoto(ABC):
+
     @abstractmethod
     def ligar(self):
         pass
+    
+    @abstractmethod
     def desligar(self):
+        pass
+    
+    @property
+    @abstractmethod
+    def marca(self):
         pass
 
 
 class ControleTV(ControleRemoto):
     def ligar(self):
         print("Ligando a TV")
+        print("ligada.")
 
     def desligar(self):
         print("Desligando a TV")
+        print("desligada")
+    
+    @property
+    def marca(self):
+        return "controle LG"
     
 class ControleArCondicionado(ControleRemoto):
     def ligar(self):
-        pass
-
+        print("ligando ar condicionado")
+        print("ligado")
+    
     def desligar(self):
-        pass
+        return super().desligar()
+    
+    @property
+    def marca(self):
+        return "ar condicionado LG"
+
     
 
+televisao = ControleTV()
+televisao.ligar()
+
+ar_condicionado = ControleArCondicionado()
+ar_condicionado.ligar()
+
+print(ar_condicionado.marca)
