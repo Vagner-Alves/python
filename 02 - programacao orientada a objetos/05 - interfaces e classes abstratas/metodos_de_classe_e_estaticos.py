5125001b37e7d575c1a16aaebda8e9dd5a3952db
Vagner-Alves <vagneralves997@gmail.com>
2025-01-06 14:38:11 -0300

    interfaces e classes abstratas em python

--- 02 - programacao orientada a objetos/05 - interfaces e classes abstratas/metodos_de_classe_e_estaticos.py
+++ 02 - programacao orientada a objetos/05 - interfaces e classes abstratas/metodos_de_classe_e_estaticos.py
@@ -4,4 +4,23 @@ Métodos de classe
 métodos de classe estão ligados a classe e não ao objeto.
 Eles tem acesso ao estado da classe, pois recebem um parametro
 que aponta para a classe e não para a instancia do objeto.
+
+Método estáticos
+
+um método estático não recebe um primeiro argumento explícito, Ele também é um método
+vinculado a classe e não ao objeto da classe. Este método não pode acessar ou modificar o estado
+da classe. Ele está presente em uma classe porque faz sentido que o método esteja presente nela.
+
+qual a diferença?
+
+um método de classe recebe um primeiro parametro que aponta para a classe,
+enquanto um método estático não.
+
+um método de classe pode acessar ou modificar o estado da classe enquanto um método estático não pode
+acessá-lo ou modificá-lo
+
+
 '''
+
+
+
