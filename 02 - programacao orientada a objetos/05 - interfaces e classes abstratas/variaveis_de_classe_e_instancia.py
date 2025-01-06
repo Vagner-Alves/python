d5b7c9b000ee82b8dca76a1e094cfac2a86c49b9
Vagner-Alves <vagneralves997@gmail.com>
2025-01-06 13:22:09 -0300

    interfaces e classes abstratas em python

---
+++ 02 - programacao orientada a objetos/05 - interfaces e classes abstratas/variaveis_de_classe_e_instancia.py
@@ -0,0 +1,8 @@
+'''
+VARIAVEIS DE OBJETO
+
+Todos os objetos nascem com o mesmo número de atributos de classe
+e de instancia. Atributos de instancia são diferentes para cada objeto
+( cada objeto tem uma cópia), já os atributos de classe
+são compartilhados entre os objetos.
+'''
