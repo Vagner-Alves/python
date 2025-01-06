b52bf706dce2218d2a560baf2ec34f4f7922e0e2
Vagner-Alves <vagneralves997@gmail.com>
2025-01-06 14:04:54 -0300

    interfaces e classes abstratas em python

---
+++ 02 - programacao orientada a objetos/05 - interfaces e classes abstratas/metodos_de_classe_e_estaticos.py
@@ -0,0 +1,7 @@
+'''
+Métodos de classe
+
+métodos de classe estão ligados a classe e não ao objeto.
+Eles tem acesso ao estado da classe, pois recebem um parametro
+que aponta para a classe e não para a instancia do objeto.
+'''
