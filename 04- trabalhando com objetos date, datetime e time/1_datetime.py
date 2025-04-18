a082349094fd44a45ec6f6507f5350b859983304
Vagner-Alves <vagneralves997@gmail.com>
2025-04-18 17:00:37 -0300

    data, hora

--- 04- trabalhando com objetos date, datetime e time/1_datetime.py
+++ 04- trabalhando com objetos date, datetime e time/1_datetime.py
@@ -1,5 +1,12 @@
-from datetime import date
+from datetime import date, datetime, time
 data = date(2025,4,18)
 print(data)
-
 print(data.today())
+
+data_hora = datetime(2018,6,22)
+print(data_hora)
+print(data_hora.today())
+
+# hora atual usando a função datetime
+hora = datetime.now()
+print(hora)
