b8449bcc4a578a7363d0c9886b79f170686ed257
Vagner-Alves <vagneralves997@gmail.com>
2025-07-16 02:14:02 -0300

    timedelta exercicios

--- 04- trabalhando com objetos date, datetime e time/04 - time_delta_3.py
+++ 04- trabalhando com objetos date, datetime e time/04 - time_delta_3.py
@@ -10,9 +10,11 @@ data_atual = datetime.now()
 
 if tipo_carro == "P":
     data_estimada = data_atual + timedelta(days = tempo_pequeno)
+    print(f"O carro chegou em: {data_atual} e ficara pronto em {data_estimada}")
 
 elif tipo_carro == "M":
     data_estimada = data_atual + timedelta(days=tempo_medio)
-
+    print(f"O carro chegou em: {data_atual} e ficara pronto em {data_estimada}")
 else:
     data_estimada = data_atual + timedelta(days=tempo_grande)
+    print(f"O carro chegou em: {data_atual} e ficara pronto em {data_estimada}")
