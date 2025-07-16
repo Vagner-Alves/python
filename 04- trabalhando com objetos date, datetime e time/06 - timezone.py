bbd95403db2700b9a12c2f42b7fc61924a184bf0
Vagner-Alves <vagneralves997@gmail.com>
2025-07-16 14:51:47 -0300

    timezones

--- 04- trabalhando com objetos date, datetime e time/06 - timezone.py
+++ 04- trabalhando com objetos date, datetime e time/06 - timezone.py
@@ -6,8 +6,11 @@ import pytz
 
 # America do Sul
 south_america_recife = datetime.now(pytz.timezone("America/Recife"))
-south_america_sao_paulo = datetime.now(pytz.timezone("America/Sao_paulo"))
 south_america_noronha = datetime.now(pytz.timezone("America/Noronha"))
+south_america_mexico = datetime.now(pytz.timezone("America/Mexico_City"))
+south_america_maceio = datetime.now(pytz.timezone("America/Maceio"))
+south_america_santo_domingo = datetime.now(pytz.timezone("America/Santo_Domingo"))
+
 # Europa
 europe_paris = datetime.now(pytz.timezone("Europe/Paris"))
 europe_madrid = datetime.now(pytz.timezone("Europe/Madrid"))
@@ -19,14 +22,25 @@ europe_lisbon = datetime.now(pytz.timezone("Europe/Lisbon"))
 north_america_vancouver = datetime.now(pytz.timezone("America/Vancouver"))
 north_america_montreal = datetime.now(pytz.timezone("America/Montreal"))
 north_america_new_york = datetime.now(pytz.timezone("America/New_York"))
+
 # Africa
+africa_abidjan = datetime.now(pytz.timezone("Africa/Abidjan"))
+africa_cairo =  datetime.now(pytz.timezone("Africa/Cairo"))
+africa_dakar = datetime.now(pytz.timezone("Africa/Dakar"))
+africa_luanda = datetime.now(pytz.timezone("Africa/Luanda"))
 
 # Asia
+asia_jakarta = datetime.now(pytz.timezone("Asia/Jakarta"))
+asia_tokyo = datetime.now(pytz.timezone("Asia/Tokyo"))
+asia_singapore = datetime.now(pytz.timezone("Asia/Singapore"))
 
 # Oceania
+oceania_west = datetime.now(pytz.timezone("Australia/West"))
+oceania_honolulu = datetime.now(pytz.timezone("Pacific/Honolulu"))
+oceania_tahiti = datetime.now(pytz.timezone("Pacific/Tahiti"))
 
 # Antartica
-
+antarctica_south_pole = datetime.now(pytz.timezone("Antarctica/South_Pole"))
 
 
 print(fuso_horario_europa)
