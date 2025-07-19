e41dfe6bf6c42082979f4ae9e59110d618ccf179
Vagner-Alves <vagneralves997@gmail.com>
2025-07-18 22:07:13 -0300

    arquivos

--- 05 - Manipulando Arquivos/01 - abrindo_arquivos.py
+++ 05 - Manipulando Arquivos/01 - abrindo_arquivos.py
@@ -3,3 +3,7 @@ Existem diferentes modos de abrir um arquivo, como somente leitura (r), gravaç�
 O modo de abertura deve ser escolhido de acordo com a operação que iremos realizar no mesmo.
 '''
 
+arquivo_texto = open("texto.txt")
+
+for texto in arquivo_texto.readlines():
+    print(texto)
