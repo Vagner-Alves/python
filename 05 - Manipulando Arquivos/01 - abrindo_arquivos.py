040566cfc5df37d043dd28102559dd72c57d6857
Vagner-Alves <vagneralves997@gmail.com>
2025-07-18 21:51:25 -0300

    manipulando arquivos

---
+++ 05 - Manipulando Arquivos/01 - abrindo_arquivos.py
@@ -0,0 +1,5 @@
+'''
+Existem diferentes modos de abrir um arquivo, como somente leitura (r), gravação (w) e anexar (a).
+O modo de abertura deve ser escolhido de acordo com a operação que iremos realizar no mesmo.
+'''
+
