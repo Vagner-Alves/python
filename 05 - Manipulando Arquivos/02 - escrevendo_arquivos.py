157df1e22d2dd334423ce353937cb1501220f047
Vagner-Alves <vagneralves997@gmail.com>
2025-07-19 20:35:53 -0300

    lendo arquivos

--- 05 - Manipulando Arquivos/02 - escrevendo_arquivos.py
+++ 05 - Manipulando Arquivos/02 - escrevendo_arquivos.py
@@ -7,9 +7,10 @@ file = open('escrita.txt', 'w')
 file.write("escrevendo com python") # é possível passar uma string
 file.close()
 
-file_2 = open('escrita.txt','w')
+file_2 = open('escrita-varias-vezes.txt','w')
 file_2.writelines(["escrevendo ", "textos ", " com python!"]) # é possível usar um iteravél ( lista ou tuplas )
 file_2.close()
+
 # lendo o contéudo do arquivo
 read_file = open('escrita.txt', 'r')
 print(read_file.readlines())
